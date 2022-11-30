e2e80d30d7af177a3f9d93cb6bfedf864431de61
JasonPC <jasonwong287962@gmail.com>
2022-11-30 02:11:12 -0600

    practice some validity in javascript

---
+++ js/validate.js
@@ -0,0 +1,48 @@
+let form = document.querySelector('form');
+let pwrd = document.getElementById('pwrd');
+let pwrd2 = document.getElementById('pwrd2');
+
+
+
+pwrd.addEventListener('input', e => {
+
+}),false;
+
+window.addEventListener('load',e => {
+ 
+   let formarray = Array.from(form);
+   formarray.forEach(element => {
+ 
+    element.className = 'empty';
+   });
+    
+});
+
+form.addEventListener('change' ,e =>{
+    e.stopPropagation
+    let formarray = Array.from(form);
+    formarray.forEach(element => { e.target.className ='';
+        if(element.className === 'empty'){
+
+        }
+        else if(element.validity.valid){
+            element.style.backgroundColor = '#beffb1';
+            element.style.borderColor = '#dadada';
+        }
+
+     
+        // if(e.target ==='pwrd'){
+
+        // }
+
+    })
+   
+});
+
+
+form.addEventListener('submit',e =>{
+    e.preventDefault();
+console.log(e);
+
+
+});
